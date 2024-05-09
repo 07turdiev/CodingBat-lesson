7355f6d068ebc2b8722a47d932519a81448106bd
Elbek <elbekturdiyev20020707@gmail.com>
2024-05-09 13:53:12 +0500

    Coding batdan yangi task bajarildi (Sum3)

---
+++ src/Array_1/Sum3.java
@@ -0,0 +1,12 @@
+package Array_1;
+
+public class Sum3 {
+    public int sum3(int[] nums) {
+        int a = 0;
+        for (int i = 0; i < nums.length; i++) {
+            a += nums[i];
+
+        }
+        return a;
+    }
+}
