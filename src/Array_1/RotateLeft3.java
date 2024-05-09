88def26471211c4c7122624a0413926bd36ccabd
Elbek <elbekturdiyev20020707@gmail.com>
2024-05-09 14:16:56 +0500

    Coding batdan yangi task bajarildi (RotateLeft3)

---
+++ src/Array_1/RotateLeft3.java
@@ -0,0 +1,11 @@
+package Array_1;
+
+public class RotateLeft3 {
+    public int[] rotateLeft3(int[] nums) {
+        int[] rotatedArray = new int[3];
+        rotatedArray[0] = nums[1];
+        rotatedArray[1] = nums[2];
+        rotatedArray[2] = nums[0];
+        return rotatedArray;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
-import warmap_1.EveryNth;
-import warmup_2.StringYak;
+import Array_1.RotateLeft3;
+
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        StringYak stringYak = new StringYak();
-        System.out.println(stringYak.stringYak("yakpak"));
+        RotateLeft3 aa = new RotateLeft3();
+        int[] a = {1, 2, 3};
+        System.out.println(Arrays.toString(aa.rotateLeft3(a)));
 
 
     }
