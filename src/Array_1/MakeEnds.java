e1473d1edb6b3c2268ca453ca8dd180de1f79eb4
Elbek <elbekturdiyev20020707@gmail.com>
2024-05-11 16:56:18 +0500

    MakeEnds to'ldirildi

--- src/Array_1/MakeEnds.java
+++ src/Array_1/MakeEnds.java
@@ -1,4 +1,10 @@
 package Array_1;
 
 public class MakeEnds {
+    public int[] makeEnds(int[] nums) {
+        int[] a = new int[2];
+        a[0] = nums[0];
+        a[1] = nums[nums.length - 1];
+        return a;
+    }
 }
