e0327294296abe3692d7b8d1bb99327df8b6cc32
Elbek <elbekturdiyev20020707@gmail.com>
2024-05-11 16:51:49 +0500

    Coding batdan yangi task bajarildi (MakeEnds)

---
+++ src/Array_1/MiddleWay.java
@@ -0,0 +1,10 @@
+package Array_1;
+
+public class MiddleWay {
+    public int[] middleWay(int[] a, int[] b) {
+        int[] d = new int[2];
+        d[0] = a[1];
+        d[1] = b[1];
+        return d;
+    }
+}
